3a9c5bdf19dffca4033115a31fd5f60b69b54135
Evolve1945 <rudolf.krylov@efrei.net>
2023-11-26 17:53:17 +0100

    Correction TF function + Cleaned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from math import log
 # -*- coding: utf-8 -*-
 
 #PART I
@@ -77,3 +78,168 @@ def president_full_names (list_of_files : list) -> None:
 
 
 president_full_names(get_names(directory))
+
+
+
+#Cleaned text in "Cleaned" folder
+
+def folder_cleaned() -> bool:
+  """
+  IN : None
+  Out : bool, True
+  Description : Create a folder named "Cleaned" if it doesn't exist, and delete all the files in it if it does already exist
+  """
+  
+  path_dir = 'Cleaned'                              # Set the path of the folder to be checked/created
+  
+  if os.path.exists(path_dir):                      # Check if the folder already exists
+    
+                                                    # If not empty, delete the files but keep the folder
+    list_of_files_name = os.listdir(path_dir)       # Get the list of file names in the folder
+    for file in list_of_files_name:                 
+      file_path = os.path.join(path_dir, file)      # Get the full path of the file
+      os.remove(file_path)                          # Remove the file
+    
+                                                    
+  else:                                             # If the folder doesn't exist
+    os.mkdir(path_dir)                              # Create the folder
+
+  return True
+
+
+
+
+def file_cleaned():
+  """
+  For folder_cleaned == True :
+  ...
+  Then verify that each chr in txt is well formated
+        If no : reformate it
+        Then put the chr in the file
+  """
+  acc = {'ç': 'c', 'é': 'e', 'è': 'e', 'ê': 'e', 'à': 'a', 'â': 'a', 'ù': 'u', 'û': 'u', 'î': 'i', 'ï': 'i', 'ô': 'o', 'ö': 'o', 'œ': 'oe', 'ç': 'c', 'É': 'E', 'È': 'E', 'Ê': 'E', 'À': 'A', 'Â': 'A', 'Ù': 'U', 'Û': 'U', 'Î': 'I', 'Ï': 'I', 'Ô': 'O', 'Ö': 'O', 'Œ': 'OE'}
+
+  path_file_orgl = "Speeches"                                 # Original path
+  path_file_prime = "Cleaned"                                 # New path
+
+  for file_name in os.listdir(path_file_orgl):                # For the file in the original folder do :
+    path_file_orgl = "Speeches"                               # Original path
+    path_file_prime = "Cleaned"  
+    path_file_orgl = os.path.join(path_file_orgl, file_name)  # path_file_orgl\file_name
+    path_file_prime = os.path.join(path_file_prime,file_name) # path_file_prime\file_name
+    #print(path_file_orgl)
+    #print(path_file_prime)
+    
+    
+    with open(path_file_orgl, 'r', encoding='utf-8') as file_orgl, open(path_file_prime, 'w', encoding='utf-8') as file_prime :
+      lines = file_orgl.readlines()                           # "Read" each line of the orginal and return them as a list of str (a line = an element)
+      
+      for line in lines:  
+        cleaned_line = ""  
+        
+        for character in line:                                # For each character in each line
+          formatted_character = character.lower()             # Lower the character A -> a
+      
+          if formatted_character in ("'", "-",):               # if f_chr = ! . ? , ; :
+              formatted_character = " "
+          elif formatted_character in (".", ",", ":", ";", "!", "?"):       
+              formatted_character = ""
+          
+          for key, value in acc.items():
+            formatted_character = formatted_character.replace(key, value) # Replace a special character by a "normal" one
+          
+          cleaned_line += formatted_character
+          
+
+        file_prime.write(cleaned_line)                 # Re-write the lowered character in the new file
+
+  return True
+
+
+"""
+1. Display the list of least important words in the document corpus. 
+A word is said to be unimportant if its TD-IDF = 0 in all files.
+2. Display the word(s) with the highest TD-IDF score
+3. Indicate the most repeated word(s) by President Chirac
+4. Indicate the name(s) of the president(s) who spoke of the "Nation" and the one who repeated it the most
+times.
+5. Identify the first president to talk about climate (“climat”) and/or ecology (“écologie”)
+6. Excepti the so-called "unimportant" words, which word(s) did all the president mention?
+"""
+
+def tf(text : str) -> dict:
+  """IN : str, the text
+  OUT : dict, the frequency of each word in the text
+  Description : Calculate how many times a word appears in a text
+  """
+  frequency = {}                                                              # Create an empty dictionary to store the word frequencies
+  text = text.split(" ")                                                      # Split the text into individual words
+  for word in text:                                                           # Iterate over each word in the text
+    if word not in frequency:                                                 # Check if the word is not already in the frequency dictionary
+      frequency[word] = 1                                                     # If not, initialize the frequency of the word to 1
+    else:
+      frequency[word] += 1                                                    # If the word is already in the frequency dictionary, increment its frequency by 1
+  frequency = dict(sorted(frequency.items(), key=lambda item: item[0]))       # Sort the frequency dictionary by word in ascending order
+  return frequency                                                             # Return the dictionary of word frequencies
+
+
+
+
+
+
+def idf(folder : str) -> dict:
+  """
+  IN : str, the path of the folder
+  OUT : dict, the idf of each word in the folder
+  Description : Function that takes a folder as input and returns a dictionary of the idf of each word in the folder
+  """
+  
+  idf_dict = {}                                                                                 # Create an empty dictionary to store the IDF values
+  nb = 0                                                                                        # Initialize a counter variable to keep track of the number of files
+  
+  for files in os.listdir(folder):                                                              # Iterate over each file in the specified folder
+    with open(os.path.join(folder, files), 'r', encoding = 'utf-8') as file:                    # Open the file in read mode with UTF-8 encoding
+      nb += 1                                                                                   # Increment the counter variable
+      words = file.read().split()                                                               # Read the contents of the file and split it into words
+      frequency = tf(words)                                                                     # Calculate the term frequency (TF) for each word in the file
+      for word in frequency:                                                                    # Iterate over each word in the frequency dictionary
+        idf_dict[word] = log(nb / frequency[word])                                              # Calculate the IDF value for the word and store it in the IDF dictionary
+  return idf_dict                                                                               # Return the IDF dictionary
+
+
+def calculate_tfidf(text, folder):
+  tfidf = {}
+  tf = tf(text)
+  idf = idf(folder)
+
+  for word in tf.keys():
+    tfidf[word] = tf[word] * idf[word]
+
+  total_idf = sum(idf.values())  # Calculate the total final idf
+
+  return tfidf, total_idf
+"""
+def unimportant_words():
+  least_important_words = []
+  for word in text:
+    if calculate_tfidf(word) == 0:
+      least_important_words.add(word)
+  return least_important_words
+    
+    
+def important_words():
+  most_important_words = []
+  for word in text:
+    if calculate_tfidf(word) == max(calculate_tfidf(text)):
+      most_important_words.add(word)
+  return most_important_words
+
+"""
+  
+# CALL
+president_full_names(get_names(directory))
+
+folder_cleaned()
+file_cleaned()
+
+print(tf("Speeches mommy car car car bobby vive la france la France est un beau pays il faut se réveiller et aller travailler pour la france".lower()))
