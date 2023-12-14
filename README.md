1ebea153202503f9f6f7af7f8e64512615a7a844
Evolve1945 <rudolfkrylov1604@gmail.com>
2023-12-14 15:25:07 +0100

    menu

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Chatbot Python Project
 KRYLOV Rudolf, POUILLAUDE Agathe
 
 To launch functions remove the tags for some tests, otherwise create tests by yourself. Be sure your python is updated. Hope you like our project.
+
+
+
+Github link : https://github.com/Evolve1945/Chatbot
--- main.py
+++ main.py
@@ -85,62 +85,6 @@ def run_chatbot():
     choice_menu(choice, "Speeches")
 
 
-
-  
-
-#print(files_names)
-
-def president_last_name(file : str) -> str:
-  """
-  IN : str, the full name of the .txt file, with the extension
-  OUT : the name of the president's name giving the speech
-  Description : print the president's name present in the title of the file (without the number and the extention file)
-  """
-  L = os.path.basename(file).split("_") 
-  """
-  Split the name of the last file/folder of the path into a list of strs, where we can 
-  find the first element, supposed   to be the word "Nomination" and the second element, 
-  the name of the president with or without a number, following with the extension of 
-  the file, here ".txt"
-  """
-  cpt = 0
-  for i in range (len(L[1].split(".")[0])-1,1,-1) :#for loop to prevent from the name
-  #being followed by a number having more than 1 digit
-    if L[1].split(".")[0][i] in "abcdefghijklmnopqrstuvwxyz" and cpt == 0: 
-      name = L[1].split(".")[0][:i+1]
-      cpt += 1
-  return name
-
-#print(president_last_name("Speeches/Nomination_Giscard dEstaing23574657642746246247642464564754.txt"))
-
-
-def get_names (directory :str) -> list :
-  list_of_speeches = []
-  for namefile in os.listdir(directory) :
-    if namefile.endswith(".txt") :
-      speech = president_last_name(os.path.join(namefile))
-      if speech not in list_of_speeches :
-        list_of_speeches.append(speech)
-  return list_of_speeches
-
-#print(get_names(directory))
-
-
-
-def president_full_names (list_of_files : list) -> None:
-  """Function that takes a list of name files as input and prints a string of with all president full names
-  IN :
-  OUT : 
-  """
-  global dict_president_full_name # Declare a global variable
-  names = [] # Declare an empty list to store the names
-  for text in list_of_files : # Loop through the list of files we gave as paramaters
-    name = dict_president_full_name[text]+ " " + text # Concatenate the president's first name and last name
-    names.append(name) # Append the name to the list of names
-  print("|", " | ".join(names), "|") # Print the list of names in a formatted way
-
-
-president_full_names(get_names(directory))
 run_chatbot()
 
 
