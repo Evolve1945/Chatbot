a8281fc0f07e9a00ff27341cbe8455450fb4c405
imtoatb <150229871+imtoatb@users.noreply.github.com>
2023-12-17 11:21:04 +0000

    Découverte problème tfidf_question

--- Part_II.py
+++ Part_II.py
@@ -282,17 +282,21 @@ def highest_tfidf_score(tfidf_matrix_question):
 
   unimportant_words_mentionned = ['c', 's', 'qu', 'suis', 'es', 'est', 'sommes', 'etes', 'sont', 'me', 'n', 'elle', 'il', 'elles', 'ils', 'soit', 'j', 'je', 'ses', 'se', 'sa', 'ca', 'l', 'le', 'les', 'la', 'un', 'une', 'd', 'de', 'du', 'des', 'et', 'ou', 'où', 'a', 'à', 'au', 'aux', 'en', 'par', 'pour', 'avec', 'dans', 'sur', 'sous', 'entre', 'vers', 'mais', 'donc', 'or', 'ni', 'car', 'que', 'qui', 'quoi', 'quand', 'comment', 'pourquoi', 'quel', 'quelle', 'quelles', 'quels', 'ce', 'cet', 'cette', 'ces', 'mon', 'ton', 'son', 'notre', 'votre', 'leur', 'ceci', 'cela', 'celui', 'celle', 'ceux', 'celles', 'ici', 'là', 'lui', 'eux', 'elles', 'si', 'tout', 'tous', 'toute', 'toutes', 'rien', 'aucun', 'aucune', 'autre', 'autres', 'même', 'mêmes', 'tel', 'telle', 'tels', 'telles', 'quelque', 'quelques', 'plusieurs', 'plus', 'autant', 'tant', 'trop', 'peu', 'beaucoup', 'moins', 'autrefois', 'aujourd', 'hui', 'demain', 'hier', 'maintenant', 'alors', 'après', 'avant', 'bientôt', 'déjà', 'ensuite', 'jamais', 'parfois', 'souvent', 'toujours', 'tard', 'tôt', 'aussi', 'donc', 'ensuite', 'puis', 'quand', 'que', 'comment', 'où', 'pourquoi', 'qui', 'quoi', 'si', 'comme', 'ainsi']
 
-  tfidf_score = tfidf_matrix_question[0][1]
+  tfidf_score = tfidf_matrix_question[0][1][0]
   tfidf_mot = tfidf_matrix_question[0][0]
+  many_word_with_same_score = []
   print(tfidf_mot, " = ", tfidf_score)
 
   for i in range(len(tfidf_matrix_question)):
-    if tfidf_matrix_question[i][1] > tfidf_score and tfidf_matrix_question[i][0] not in unimportant_words_mentionned:
-      tfidf_score = tfidf_matrix_question[i][1]
+    if tfidf_matrix_question[i][1][0] > tfidf_score and tfidf_matrix_question[i][0] not in unimportant_words_mentionned:
+      tfidf_score = tfidf_matrix_question[i][1][0]
+      print(tfidf_matrix_question[i][0], " = ", tfidf_score)
     
   for j in range(len(tfidf_matrix_question)):
-    if tfidf_score == tfidf_matrix_question[j][1]:
+    if tfidf_score == tfidf_matrix_question[j][1][0]:
+      many_word_with_same_score.append(tfidf_matrix_question[j][0])
       tfidf_mot = tfidf_matrix_question[j][0]
+      print(many_word_with_same_score)
       print(tfidf_mot, " = ", tfidf_score)
 
   
@@ -300,7 +304,27 @@ def highest_tfidf_score(tfidf_matrix_question):
 
 word_in_question = words_in_question(input("Enter a question : "))
 tdidf_question = calculate_tdidf_question(word_in_question)
-print(highest_tfidf_score(tdidf_question))
+print(tdidf_question)
+print(highest_tfidf_score(tdidf_question)) #Problème avec la phrase "La France est-elle écologique ?" -> print competition  =  0, est  =  2.237877677471286, est  =  2.237877677471286, france  =  2.237877677471286, 2.237877677471286
+
+
+"""
+Probleme avec tfidf_question_matrix :
+Renvoie =
+france  =  0.11778303565638346
+france  =  0.9422642852510676
+ecologique  =  1.5040773967762742
+france  =  1.531179463532985
+france  =  2.237877677471286
+['est']
+est  =  2.237877677471286
+['est', 'france']
+france  =  2.237877677471286
+['est', 'france', 'est']
+est  =  2.237877677471286
+2.237877677471286
+-> France plusieurs fois avec différent coef (en fonction textes ?)
+"""
 
 
 
